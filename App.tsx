635397796f5339e0beb715b1d132500a5fdbe433
Daniel Friyia <dfriyia@gmail.com>
2022-08-06 11:41:09 -0400

    Next step complete

--- App.tsx
+++ App.tsx
@@ -1,50 +1,84 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
-
-import {spline} from '@georgedoescode/spline';
 import {
   Canvas,
-  LinearGradient,
   Path,
   useClockValue,
   useComputedValue,
   useValue,
-  vec,
 } from '@shopify/react-native-skia';
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+import {spline} from '@georgedoescode/spline';
 import {createNoise2D} from 'simplex-noise';
 
 const App = () => {
-  const points = useValue(createPoints());
+  const noise = createNoise2D();
+  const noiseStep = 0.005;
+  const clock = useClockValue();
 
-  function createPoints() {
-    const points = [];
+  const createPoints = () => {
+    const newPoints = [];
     const numPoints = 6;
     const angleStep = (Math.PI * 2) / numPoints;
     const rad = 110;
 
     for (let i = 1; i <= numPoints; i++) {
       const theta = i * angleStep;
-
       const x = 130 + Math.cos(theta) * rad;
       const y = 130 + Math.sin(theta) * rad;
 
-      // store the point
-      points.push({
-        x: x,
-        y: y,
+      newPoints.push({
+        x,
+        y,
         originX: x,
         originY: y,
-        noiseOffsetX: Math.random() * 1000,
-        noiseOffsetY: Math.random() * 1000,
+        noiseOffsetX: x,
+        noiseOffsetY: y,
       });
     }
 
-    return points;
-  }
+    return newPoints;
+  };
+
+  const mapNumbers = (
+    n: number,
+    start1: number,
+    end1: number,
+    start2: number,
+    end2: number,
+  ) => {
+    return ((n - start1) / (end1 - start1)) * (end2 - start2) + start2;
+  };
+
+  const animate = () => {
+    const newPoints = [];
+
+    for (let i = 0; i < points.current.length; i++) {
+      const point = points.current[i];
+
+      const nX = noise(point.noiseOffsetX, point.noiseOffsetX);
+      const nY = noise(point.noiseOffsetY, point.noiseOffsetY);
+      const x = mapNumbers(nX, -1, 1, point.originX - 20, point.originX + 20);
+      const y = mapNumbers(nY, -1, 1, point.originY - 20, point.originY + 20);
+
+      point.x = x;
+      point.y = y;
+
+      point.noiseOffsetX += noiseStep;
+      point.noiseOffsetY += noiseStep;
+
+      newPoints.push(point);
+    }
+
+    points.current = newPoints;
+  };
+
+  const points = useValue(createPoints());
 
   const path = useComputedValue(() => {
+    animate();
     return spline(points.current, 1, true);
-  }, []);
+  }, [clock]);
 
   return (
     <SafeAreaView style={styles.container}>
